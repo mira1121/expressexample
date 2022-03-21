874806f3fc234e6dd7bf2f55a65ff9f09f6974a9
Munkhjin <munkhjin@gerege.mn>
2022-03-21 18:16:34 +0800

    a

--- src/model/table.js
+++ src/model/table.js
@@ -1,25 +1,33 @@
 module.exports = (sequelize, DataTypes) => {
-  return sequelize.define("table", {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
-      allowNull: false,
+  return sequelize.define(
+    "table",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      xid: {
+        type: DataTypes.INTEGER,
+        primaryKey: false,
+        allowNull: false,
+      },
+      yid: {
+        type: DataTypes.INTEGER,
+        primaryKey: false,
+        allowNull: false,
+      },
+      dataid: {
+        type: DataTypes.STRING(255),
+        primaryKey: false,
+        allowNull: false,
+      },
     },
-    xid: {
-      type: DataTypes.INTEGER,
-      primaryKey: false,
-      allowNull: false,
-    },
-    yid: {
-      type: DataTypes.INTEGER,
-      primaryKey: false,
-      allowNull: false,
-    },
-    dataid: {
-      type: DataTypes.STRING(255),
-      primaryKey: false,
-      allowNull: false,
-    },
-  });
+    {
+      timestamps: false,
+      freezeTableName: true,
+      // tableName: "table",
+    }
+  );
 };
